4acdaeb8597f3960ab5fdb916605967a13917868
Ziya Emanet <ziyaemanet@gmail.com>
2016-10-23 04:30:37 -0700

    transactions11

--- server/models/Tropo.js
+++ server/models/Tropo.js
@@ -40,12 +40,11 @@ exports.trans = (req, res) => {
 
       if (members[call.sender] !== undefined) {
 
-        // var edit = banks.filter((curr) => curr.chair == members[call.sender]);
-        let edit = ['test array'];
-        edit.transactions.push(call.input);
+        var edit = banks.filter((curr) => curr.chair == members[call.sender]);
+        edit.push(call.input);
         console.log('edit: ', edit);
         return Bank.findOneAndUpdate({ chair: members[call.sender] },
-                                     { $set: { transactions: edit.transactions } });
+                                     { $set: { transactions: edit } });
       } else {
         console.log('in else');
         return res.end();
